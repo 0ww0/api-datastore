4f0f58037af055bf6bcb09d716346d1a866ee156
0ww0 <shah@ping-u.com.my>
2020-10-19 14:31:58 +0800

    User API

---
+++ config/auth.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'defaults' => [
+        'guard' => 'api',
+        'passwords' => 'users',
+    ],
+
+    'guards' => [
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+        ],
+    ],
+
+    'passwords' => [
+        //
+    ],
+
+    'providers' => [
+        'users' => [
+            'driver' => 'eloquent',
+            'model' => \App\Models\User::class
+        ]
+    ]
+];
